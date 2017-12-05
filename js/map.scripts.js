f8ff017360387c134529c735d1a3a11d9bf00053
Mir Suhail <suhail.mir.ahmed@gmail.com>
2017-12-05 19:38:54 +0530

    Services Completed

--- js/map.scripts.js
+++ js/map.scripts.js
@@ -6,133 +6,133 @@ function init() {
 	
 	var mapOptions = {
 		zoom: 14,
-		center: new google.maps.LatLng(34.084238, 74.819559), // ECC , 
-		styles: 
-		[
-			{
-				"featureType": "administrative",
-				"elementType": "all",
-				"stylers": [
-					{
-						"saturation": "-100"
-					}
-				]
-			},
-			{
-				"featureType": "administrative.province",
-				"elementType": "all",
-				"stylers": [
-					{
-						"visibility": "off"
-					}
-				]
-			},
-			{
-				"featureType": "landscape",
-				"elementType": "all",
-				"stylers": [
-					{
-						"saturation": -100
-					},
-					{
-						"lightness": 65
-					},
-					{
-						"visibility": "on"
-					}
-				]
-			},
-			{
-				"featureType": "poi",
-				"elementType": "all",
-				"stylers": [
-					{
-						"saturation": -100
-					},
-					{
-						"lightness": "50"
-					},
-					{
-						"visibility": "simplified"
-					}
-				]
-			},
-			{
-				"featureType": "road",
-				"elementType": "all",
-				"stylers": [
-					{
-						"saturation": "-100"
-					}
-				]
-			},
-			{
-				"featureType": "road.highway",
-				"elementType": "all",
-				"stylers": [
-					{
-						"visibility": "simplified"
-					}
-				]
-			},
-			{
-				"featureType": "road.arterial",
-				"elementType": "all",
-				"stylers": [
-					{
-						"lightness": "30"
-					}
-				]
-			},
-			{
-				"featureType": "road.local",
-				"elementType": "all",
-				"stylers": [
-					{
-						"lightness": "40"
-					}
-				]
-			},
-			{
-				"featureType": "transit",
-				"elementType": "all",
-				"stylers": [
-					{
-						"saturation": -100
-					},
-					{
-						"visibility": "simplified"
-					}
-				]
-			},
-			{
-				"featureType": "water",
-				"elementType": "geometry",
-				"stylers": [
-					{
-						"hue": "#ffff00"
-					},
-					{
-						"lightness": -25
-					},
-					{
-						"saturation": -97
-					}
-				]
-			},
-			{
-				"featureType": "water",
-				"elementType": "labels",
-				"stylers": [
-					{
-						"lightness": -25
-					},
-					{
-						"saturation": -100
-					}
-				]
-			}
-		]
+		center: new google.maps.LatLng(34.084238, 74.819559) // ECC , 
+		//styles: 
+		//[
+		//	{
+		//		"featureType": "administrative",
+		//		"elementType": "all",
+		//		"stylers": [
+		//			{
+		//				"saturation": "-100"
+		//			}
+		//		]
+		//	},
+		//	{
+		//		"featureType": "administrative.province",
+		//		"elementType": "all",
+		//		"stylers": [
+		//			{
+		//				"visibility": "off"
+		//			}
+		//		]
+		//	},
+		//	{
+		//		"featureType": "landscape",
+		//		"elementType": "all",
+		//		"stylers": [
+		//			{
+		//				"saturation": -100
+		//			},
+		//			{
+		//				"lightness": 65
+		//			},
+		//			{
+		//				"visibility": "on"
+		//			}
+		//		]
+		//	},
+		//	{
+		//		"featureType": "poi",
+		//		"elementType": "all",
+		//		"stylers": [
+		//			{
+		//				"saturation": -100
+		//			},
+		//			{
+		//				"lightness": "50"
+		//			},
+		//			{
+		//				"visibility": "simplified"
+		//			}
+		//		]
+		//	},
+		//	{
+		//		"featureType": "road",
+		//		"elementType": "all",
+		//		"stylers": [
+		//			{
+		//				"saturation": "-100"
+		//			}
+		//		]
+		//	},
+		//	{
+		//		"featureType": "road.highway",
+		//		"elementType": "all",
+		//		"stylers": [
+		//			{
+		//				"visibility": "simplified"
+		//			}
+		//		]
+		//	},
+		//	{
+		//		"featureType": "road.arterial",
+		//		"elementType": "all",
+		//		"stylers": [
+		//			{
+		//				"lightness": "30"
+		//			}
+		//		]
+		//	},
+		//	{
+		//		"featureType": "road.local",
+		//		"elementType": "all",
+		//		"stylers": [
+		//			{
+		//				"lightness": "40"
+		//			}
+		//		]
+		//	},
+		//	{
+		//		"featureType": "transit",
+		//		"elementType": "all",
+		//		"stylers": [
+		//			{
+		//				"saturation": -100
+		//			},
+		//			{
+		//				"visibility": "simplified"
+		//			}
+		//		]
+		//	},
+		//	{
+		//		"featureType": "water",
+		//		"elementType": "geometry",
+		//		"stylers": [
+		//			{
+		//				"hue": "#ffff00"
+		//			},
+		//			{
+		//				"lightness": -25
+		//			},
+		//			{
+		//				"saturation": -97
+		//			}
+		//		]
+		//	},
+		//	{
+		//		"featureType": "water",
+		//		"elementType": "labels",
+		//		"stylers": [
+		//			{
+		//				"lightness": -25
+		//			},
+		//			{
+		//				"saturation": -100
+		//			}
+		//		]
+		//	}
+		//]
 	};
 
 	var mapElement = document.getElementById('map');
@@ -142,7 +142,7 @@ function init() {
 	var marker = new google.maps.Marker({
 	    position: new google.maps.LatLng(34.084238, 74.819559),
 		map: map,
-		icon: 'http://unicoderbd.com/theme/html/patron/images//map-marker.png',
-		title: 'Snazzy!'
+		icon: 'images/map-marker.png',
+		title: 'Eduvert Career Consultants!'
 	});
 }
